9524014dd1fe6197b2cd56375bcb90e1231ef865
Atley St. Vrain <atleystvrain@gmail.com>
2023-03-01 12:08:41 -0600

    Date somewhat complete

--- Date.java
+++ Date.java
@@ -1,5 +1,33 @@
 public class Date {
-    public static void main(String[] args) {
+    private int month;
+    private int day;
+    private int year;
+
+    public Date(int curMonth, int curDay, int curYear) {
+        month = curMonth;
+        day = curDay;
+        year = curYear;
+    }
+
+    public String toString() {
+        String result = "";
+        if (month < 10) {
+            result+="0"+month+"/";
+        } else {
+            result+=month+"/";
+        }
+        if (day < 10) {
+            result+="0"+day+"/";
+        } else {
+            result+=day+"/";
+        }
+        result+=year;
+
+        return result;
         
+    }
+    public static void main(String[] args) {
+        Date today = new Date(3,1,2023);
+        System.out.println(today);
     }
 }
